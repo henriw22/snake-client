cc8ef14e92d40ba58035206f7f158843dcbaea0f
henriw22 <henriw22@gmail.com>
2021-06-03 01:26:58 +0000

    exporting the setupInput function to input.js

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
 // const net = require("net");
-const connect = require("./client.js");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    // your code here
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-
+const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
