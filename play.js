515795eaf218fe0bef8746d9eaf3f78ef15b2d16
henriw22 <henriw22@gmail.com>
2021-06-03 01:07:19 +0000

    added setupInput and handleUserInput functions to handle keyboard commands

--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 // const net = require("net");
 const connect = require("./client.js");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    // your code here
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
 
 console.log("Connecting ...");
 connect();
+setupInput();
