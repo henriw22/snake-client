c7157eb53fd768158bec0856cf4194171049cded
henriw22 <henriw22@gmail.com>
2021-06-03 02:15:04 +0000

    completed the moving commands setup to play the snake game

--- input.js
+++ input.js
@@ -1,14 +1,32 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  const handleUserInput = function (key) {
+  const handleUserInput = (key) => {
     // your code here
-    if (key === '\u0003') {
+    if (key === 'w') {
+      conn.write('Move: up');
+    } else if (key === 's') {
+      conn.write('Move: down');
+    } else if (key === 'a') {
+      conn.write('Move: left');
+    } else if (key === 'd') {
+      conn.write('Move: right');
+    } else if (key === '\u0003') {
       process.exit();
     }
+    // switch (key) {
+    //   case 'w': conn.write('Move: up'); break;
+    //   case 's': conn.write('Move: down'); break;
+    //   case 'a': conn.write('Move: left'); break;
+    //   case 'd': conn.write('Move: right'); break;
+    //   case '\u0003':process.exit();
+    // }
   };
 
   stdin.on("data", handleUserInput);
